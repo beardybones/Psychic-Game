c09ab260582612f2cc572fc8e7635edc8bf85838
beardybones <beardybonescodes@gmail.com>
2018-11-09 22:22:42 -0800

    Homework 2-3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,75 @@
-    
+
 //Variables
-    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    var wins = 0;
-    var losses = 0;
-    //user gets 9 guesses to match letter
-    var guesses = 9;
-    var lettersGuessed;
-    var computerGuess;
-    var userArray = [];
+var wins = 0;
+var losses = 0;
+//user gets 9 guesses to match letter
+var guesses = 9;
+var lettersGuessed = "";
+var compGuess;
+var userArray = [];
 
-    function updateScore() {
-        document.querySelector("#Wins").innerHTML = "Wins: " + wins;
-        document.querySelector("#Losses").innerHTML = "Losses: " + losses;
-        document.querySelector("#Guesses").innerHTML = "Guesses: " + guesses;
-        document.querySelector("#Letters").innerHTML = "Letters Guessed: " + lettersGuessed;
-      }
+function updateScore() {
+    document.querySelector("#Wins").innerHTML = "Wins: " + wins;
+    document.querySelector("#Losses").innerHTML = "Losses: " + losses;
+    document.querySelector("#Guesses").innerHTML = "Guesses: " + guesses;
+    document.querySelector("#Letters").innerHTML = "Letters Guessed: " + lettersGuessed;
+}
 
-    function computerGuess() {computerGuess = letters[Math.floor(Math.random() * letters.length)];
+function computerGuess() {
+    compGuess = letters[Math.floor(Math.random() * letters.length)];
 
-    }
+}
 
 
-    updateScore();
-    computerGuess();
+updateScore();
+computerGuess();
 
 
 //comp picks random letter
-    //var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    console.log(computerGuess);
+//var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log(compGuess);
 
 //user picks random letter
-    document.onkeyup = function(event) {
-        var userGuess = event.key.toLowerCase();
-        console.log(userGuess);
-        console.log(wins);
-        //console.log(losses);
-        console.log(guesses);
-//if user matches the comp pick
-//up win count by 1
-        if (userGuess === computerGuess) {
-          wins++;
-          guesses=9;
-          updateScore();
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    console.log(wins);
+    //console.log(losses);
+    console.log(guesses);
+    //if user matches the comp pick
+    //up win count by 1
+    if (userGuess === compGuess) {
+        wins++;
+        guesses = 9;
+        updateScore();
+        computerGuess();
 
-        }
-
-//if user does not match comp pick
-//decrease guesses by 1
-        else {
-            guesses--;
-            updateScore();
-        }
-
-        if (guesses<1) {
-            losses++;
-            guesses=9
-            
-            updateScore();
-        }
-        //need to reset guesses somehow when losses go up by 1
-        
     }
+
+    //if user does not match comp pick
+    //decrease guesses by 1
+    else {
+        guesses--;
+
+        // everytime someone guesses 
+        // log the keystroke to the screen
+        lettersGuessed += userGuess + ", ";
+        console.log(lettersGuessed)
+        updateScore();
+
+
+    }
+
+    if (guesses < 1) {
+        losses++;
+        guesses = 9
+        lettersGuessed = ""
+        computerGuess();
+        updateScore();
+
+    }
+
+
+}
